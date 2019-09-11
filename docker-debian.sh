23316a7d50dd22f923b007a2026fb5196f8104dc
Larissa <37162993+lariskovski@users.noreply.github.com>
2019-09-11 09:50:36 -0300

    Update docker-debian.sh
    fixed the script

--- docker-debian.sh
+++ docker-debian.sh
@@ -1,5 +1,3 @@
-!#/bin/bash
-
 #author: Larissa
 #description: script to install docker in a container runnin debian (jenkins/jenkins)
 
@@ -9,10 +7,10 @@ apt update && sudo apt upgrade
 apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
 
 #Import the repository’s GPG key using the following curl command:
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
 #Add the Docker APT repository to your system’s software repository list
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 #Now that the Docker repository is enabled, update the apt package list and install the latest version of Docker CE (Community Edition) with:
-apt updatesudo && apt install docker-ce
+apt update && apt install docker-ce
